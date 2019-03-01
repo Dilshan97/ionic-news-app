b8b4abef39604e2ea992ff4df5ddf7c1fc7a8414
Dilshan <dilshanramesh81@gamil.com>
2019-03-01 23:10:55 +0530

    first commit

---
+++ src/app/news/news.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../service/news.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.page.html',
+  styleUrls: ['./news.page.scss'],
+})
+
+export class NewsPage implements OnInit {
+  data: any;
+  constructor(private newsService: NewsService, private _router: Router) { }
+
+  ngOnInit() {
+    this.newsService.getNews('everything?q=cricket&sortBy=publishedAt').subscribe(
+      data => { this.data = data; console.log(JSON.stringify(this.data)); }
+    )
+  }
+  goToSinglePage(news) {
+    this.newsService.currentNews = news;
+    this._router.navigate(['/tabs/news-detail']);
+  }
+}
